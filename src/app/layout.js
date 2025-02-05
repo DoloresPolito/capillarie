992120b1005bd46068e923619abde5ac486df638
Dolores Polito <dolores.polito@gmail.com>
2025-02-05 09:49:23 -0300

    change tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -62,30 +62,22 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} ${montserrat.className} ${plus.className}`}
       >
-        {/* Google Tag Manager */}
         <Script
-          id="gtm-script"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TRZ5C7R66Y"
+        />
+        <Script
+          id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NKPKQCG8');`,
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-TRZ5C7R66Y');
+    `,
           }}
         />
-        {/* End Google Tag Manager */}
-
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NKPKQCG8"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
         {children}
 
